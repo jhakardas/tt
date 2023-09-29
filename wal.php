9387d7ef0afdd7c852adbc10cc3fa8ba472d3c85
jhakardas <127119460+jhakardas@users.noreply.github.com>
2023-09-29 18:10:58 +0530

    Add files via upload

---
+++ wal.php
@@ -0,0 +1,7 @@
+<?php
+
+header('Location: https://www.walmart.com/blocked?url=L2lwL0JldHRlci1Ib21lcy1HYXJkZW5zLVNwcmluZ3dvb2QtQ2FuZS1UVi1TdGFuZC1mb3ItVFYtcy11cC10by02NS1DaGFyY29hbC1GaW5pc2gvMTM4NzQ0NzM3NQ==&uuid=961c1e4b-5ec3-11ee-a6fd-b16edbd60378&vid=&g=b');
+
+exit;
+
+?>
